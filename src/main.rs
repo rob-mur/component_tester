844bd2da7519257406b492da64f71ac6f1a835c8
Rob Murphy <robertsean.murphy@willistowerswatson.com>
2022-06-09 14:06:13 +0200

    component tester setup

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,6 @@ fn main() {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(component_tester::TemplateApp::new(cc))),
     );
 }
